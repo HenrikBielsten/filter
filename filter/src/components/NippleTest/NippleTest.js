2252f9e6d1695ff701724f8f28f507fc7d9773d7
HenrikBielsten <h.bielsten@hotmail.se>
2018-09-19 13:08:12 +0200

    almost finished with the joystick

--- filter/src/components/NippleTest/NippleTest.js
+++ filter/src/components/NippleTest/NippleTest.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
 import ReactNipple from 'react-nipple';
-import 'react-nipple/lib/styles.css';
 
 class NippleTest extends Component {
+
+  state = {
+    upDown: 0,
+  }
+
+  constructor(props){
+        super(props);
+        this.timer = null;
+    }
+
+  // pressure = (evt,data) => {
+  //   console.log(evt);
+  // }
+
+  scroll = (evt, data) => {
+    this.stopScroll();
+    console.log(data);
+    console.log(data.direction);
+    if (data.direction === undefined) {
+      this.setState({
+        upDown: 0,
+      })
+    }
+    else if (data.direction.y === 'down') {
+      this.setState({
+        upDown: 1,
+      })
+    } else if (data.direction.y === 'up') {
+      this.setState({
+        upDown: -1,
+      })
+    }
+    console.log(this.state.upDown);
+    window.scrollBy(0, this.state.upDown); // horizontal and vertical scroll increments
+    this.timer = setTimeout(() => this.scroll(evt,data),100); // scrolls every X milliseconds
+  }
+
+  stopScroll = () => {
+      clearTimeout(this.timer);
+      console.log('stopped');
+  }
+
     render() {
         return (
             <div>
@@ -13,14 +54,18 @@ class NippleTest extends Component {
                     // any unknown props will be passed to the container element, e.g. 'title', 'style' etc
                     style={{
                         outline: '1px solid red',
-                        width: 100,
-                        height: 100,
-                        position: 'absolute',
+                        width: '20vh',
+                        height: '20vh',
+                        position: 'fixed',
+                        top: '68vh',
+                        left: '60vw',
                         // if you pass position: 'relative', you don't need to import the stylesheet
                     }}
                     // all events supported by nipplejs are available as callbacks
                     // see https://github.com/yoannmoinet/nipplejs#start
-                    onMove={(evt, data) => console.log(evt, data)}
+
+                      onMove={this.scroll}
+                      onEnd={this.stopScroll}
                 />
             </div>
         );
