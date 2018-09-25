7ba34a174991d9cab0522ba32ebedcfbd51b70b7
kajsataipale <kajsataipale@gmail.com>
2018-09-25 13:49:42 +0200

    continued with the header component

--- filter/src/components/Header/Header.js
+++ filter/src/components/Header/Header.js
@@ -2,18 +2,33 @@ import React, { Component } from "react";
 import './index.css';
 import ProgressBar from '../ProgressBar/ProgressBar.js';
 class Header extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     navbar: false
+   };
+   this.eventHandler = this.eventHandler.bind(this);
+ }
+ eventHandler(event) {
+   this.setState(prevState => ({
+     navbar: !prevState.navbar
+   }));
+ }
 
 render() {
   return (
     <div className="wrapper">
       <div className="header">
         <img className="logo" src={window.location.origin + '/images/Filter_logo.svg'}  alt="logo"/>
+        <div
+        className="hamburger" >
+          <div
+            className={this.state.navbar ? "bars Active" : "bars "}
+            onClick={this.eventHandler} >
+          </div>
+         </div>
       </div>
       <ProgressBar />
-      <div className="hamburger">
-        <div></div>
-       </div>
-
     </div>
   )
 }
