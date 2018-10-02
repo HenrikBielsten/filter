b7e8de25d231354868178f142f6be1c19baebe7c
kajsataipale <kajsataipale@gmail.com>
2018-10-02 11:23:53 +0200

    added the header to the homecomponent

--- filter/src/App.js
+++ filter/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import './index.css';
-import Home from "./components/Home/Home";
+import Home from "./components/Home/Home.js";
 import Chapter1 from "./components/Chapter1/Chapter1";
 import Chapter2 from "./components/Chapter2/Chapter2";
 import Chapter3 from "./components/Chapter3/Chapter3";
 import Chapter4 from "./components/Chapter4/Chapter4";
-import Header from "./components/Header/Header.js";
 
 import { Router, Link } from '@reach/router';
 
@@ -16,7 +15,6 @@ class App extends Component {
     return (
       <div className="App">
         <nav>
-          <Header />
           <Link to="/" style={{margin: '10px'}}>Home</Link>
           <Link to="chapter1" style={{margin: '10px'}}>Chapter1</Link>
           <Link to="chapter2" style={{margin: '10px'}}>Chapter2</Link>
