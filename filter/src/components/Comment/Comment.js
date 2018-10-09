b5014e8dc6e2901ea13327b06ef07c3747f85c01
kajsataipale <kajsataipale@gmail.com>
2018-10-09 12:13:56 +0200

    fixed the function in the commetn component

--- filter/src/components/Comment/Comment.js
+++ filter/src/components/Comment/Comment.js
@@ -11,7 +11,7 @@ class Comment extends Component {
  eventHandler(event) {
    event.stopPropagation();
    this.setState(prevState => ({
-     readcomment: !prevState.navbar
+     readcomment: !prevState.readcomment
    }));
  }
 
